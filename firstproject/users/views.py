23a9b44823d59e6415e6c0c97c6786d9ef10e1c1
Coding-Cooking <fei.coding2023@gmail.com>
2024-09-10 21:41:30 +1000

    update login feature

--- firstproject/users/views.py
+++ firstproject/users/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def register_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            login(request, form.save())
             return redirect("posts:list")
     else:
         form=UserCreationForm()
@@ -16,6 +17,8 @@ def login_view(request):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
+            # login here
+            login(request, form.get_user())
             return redirect("posts:list")
     else:
         form = AuthenticationForm()
